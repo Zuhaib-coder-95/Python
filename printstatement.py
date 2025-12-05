e201919e18258943afc927ec25998651a32e8b1c
Zuhaib-coder-95 <ishaqsyd.gm@gmail.com>
2025-12-05 15:12:50 +0400

    Add files via upload

---
+++ printstatement.py
@@ -0,0 +1,11 @@
+#Printing my name 
+print("My name is Zuhaib")
+
+#Printing my Class
+print("I am in Class 8th")
+
+#Printing my hobby
+print("I like coding")
+
+#Printing a simple math operation
+print("The sum of 3 and 4 is:", 3 + 4)
