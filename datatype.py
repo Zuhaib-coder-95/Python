7c2da539619017e04c49973d24decf245ab524be
Zuhaib-coder-95 <ishaqsyd.gm@gmail.com>
2025-12-12 14:46:23 +0400

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,12 @@
+
+age = 7
+print("Type of a: ", type(age))
+
+weight = 39.7
+print("Type of b: ", type(weight))
+
+name = "Sam"
+print("Type of c: ", type(name))
+
+isMale = True
+print("Type of d: ", type(isMale))
