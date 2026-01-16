52c5ef8cb14b68dfae2b693dc8e0acf187a91fb5
Zuhaib-coder-95 <ishaqsyd.gm@gmail.com>
2026-01-16 15:11:15 +0400

    Add files via upload

---
+++ reverse_string.py
@@ -0,0 +1,7 @@
+mystring = input("Enter the string you want to reverse:")
+reveresedstring = ""
+
+for letter in mystring:
+    reveresedstring = letter + reveresedstring
+
+print("The reversed string=", reveresedstring)
