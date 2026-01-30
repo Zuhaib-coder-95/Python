f99cc0a40e30a8fd1a0f99e02997a3e981b0145d
Zuhaib-coder-95 <ishaqsyd.gm@gmail.com>
2026-01-30 15:12:33 +0400

    Add files via upload

---
+++ rightangle..py
@@ -0,0 +1,8 @@
+print("Half Pyramid Pattern of stars (*):")
+rows = int(input("Enter the number of rows: "))
+
+for i in range (rows):
+
+    for j in range (i+1):
+        print("* ", end="")
+    print()
