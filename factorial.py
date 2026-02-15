6c3e3d8fbdab1499849d56f4b5b91e38f11bcf3d
Zuhaib-coder-95 <ishaqsyd.gm@gmail.com>
2026-02-15 14:56:38 +0400

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,12 @@
+def factorial(x):
+    '''This is a recursive function to find the factorial of an integer'''
+
+    if  x == 0 or x == 1:
+        return 1
+    
+    else: 
+        return x * factorial (x-1)
+    
+print(factorial.__doc__)
+print("The factorial of 0 is ", factorial(0))
+print("The factorial of 5 is ", factorial(5))
